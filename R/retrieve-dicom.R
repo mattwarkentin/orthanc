686d83b1a0a2b71cd0478bc030e21b7cb0e32ba1
Matthew T. Warkentin <warkentin@lunenfeld.ca>
2026-03-02 10:35:48 -0700

    Add ability to stream resources when writing patients to disk

--- R/retrieve-dicom.R
+++ R/retrieve-dicom.R
@@ -6,11 +6,14 @@
 #'   been used to set persistent background processes, this function will write
 #'   patients to disk in parallel using all available processes.
 #'
-#' @param patients List of [Patient]s
+#' @param patients List of [Patient]s.
 #' @param path Path where you want to write the patients (files).
 #' @param progress Whether to show progress bars. By default, progress bars are
 #'   enabled in interactive sessions (i.e., if `rlang::is_interactive()`
 #'   returns `TRUE`).
+#' @param stream Should the resources be streamed and written to disk in
+#'   chunks? Default is `FALSE`, which means the resource file contents are
+#'   retrieved in their entirety and written to disk all at once.
 #'
 #' @return Nothing, invisibly.
 #'
@@ -27,61 +30,56 @@
 retrieve_and_write_patients = function(
   patients,
   path,
+  stream = FALSE,
   progress = rlang::is_interactive()
 ) {
-  check_list(patients)
-  check_scalar_character(path)
+  check_list_of_class_any(patients, "Patient")
+  check_path_exists(path)
   check_scalar_logical(progress)
+  check_scalar_logical(stream)
 
-  if (!fs::dir_exists(path)) {
-    rlang::abort("`path` does not exist.")
-  }
   purrr::walk(
     .x = patients,
     .f = purrr::in_parallel(
-      .f = \(x) retrieve_and_write_patient(x, path),
+      .f = \(x) retrieve_and_write_patient(x, path, stream),
       path = path,
       retrieve_and_write_patient = retrieve_and_write_patient,
       retrieve_and_write_study = retrieve_and_write_study,
       retrieve_and_write_series = retrieve_and_write_series,
-      retrieve_and_write_instance = retrieve_and_write_instance
+      retrieve_and_write_instance = retrieve_and_write_instance,
+      stream = stream
     ),
     .progress = ifelse(progress, "Exporting Patients", FALSE)
   )
   invisible()
 }
 
-retrieve_and_write_patient = function(patient, path) {
+retrieve_and_write_patient = function(patient, path, stream) {
   patient_id <- patient$patient_id
   if (patient_id == "" || rlang::is_null(patient_id)) {
     patient_id <- "unknown"
   }
   patient_path <- glue::glue("{path}/{patient_id}")
-  purrr::walk(patient$studies, \(x) retrieve_and_write_study(x, patient_path))
-}
-
-retrieve_and_write_study = function(study, patient_path) {
-  study_path <- glue::glue("{patient_path}/{study$uid}")
-  purrr::walk(study$series, \(x) retrieve_and_write_series(x, study_path))
-}
-
-retrieve_and_write_series = function(series, study_path) {
-  series_path <- glue::glue("{study_path}/{series$uid}")
-  fs::dir_create(series_path, recurse = TRUE)
-  purrr::walk(series$instances, \(x) {
-    retrieve_and_write_instance(x, series_path)
+  purrr::walk(patient$studies, \(x) {
+    retrieve_and_write_study(x, patient_path, stream)
   })
 }
 
-retrieve_and_write_instance = function(instance, series_path) {
-  instance_path <- fs::path_expand(glue::glue(
-    "{series_path}/{instance$uid}.dcm"
-  ))
+retrieve_and_write_study = function(study, patient_path, stream) {
+  study_path <- glue::glue("{patient_path}/{study$uid}")
+  purrr::walk(study$series, \(x) {
+    retrieve_and_write_series(x, study_path, stream)
+  })
+}
 
-  file_con <- file(instance_path, "wb")
-  on.exit(close(file_con))
+retrieve_and_write_series = function(series, study_path, stream) {
+  series_path <- glue::glue("{study_path}/{series$uid}")
+  fs::dir_create(series_path, recurse = TRUE)
+  purrr::walk(series$instances, \(x) {
+    retrieve_and_write_instance(x, series_path, stream)
+  })
+}
 
-  dicom_instance_bytes <- instance$get_dicom_file_content()
-
-  writeBin(as.raw(dicom_instance_bytes), file_con)
+retrieve_and_write_instance = function(instance, series_path, stream) {
+  instance$download_dicom(series_path, stream)
 }
